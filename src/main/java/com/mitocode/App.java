11b96778cb5bb1eda56e6b5b57dd2a19f31ad762
Luis Ramirez <luisystems12@gmail.com>
2020-01-26 01:16:30 -0500

    cambio de mensaje en consola

--- src/main/java/com/mitocode/App.java
+++ src/main/java/com/mitocode/App.java
@@ -3,7 +3,7 @@ package com.mitocode;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Luis Ramírez!");
+		System.out.println("Soy Luis Ramírez...!!!");
 	}
 
 }
